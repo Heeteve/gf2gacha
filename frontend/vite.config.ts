4d6261e27cd4df90e14bad2f16c300e5adb25472
Matcha <admin@mcc.wiki>
2025-03-07 19:41:49 +0800

    chore:vite升级到v6,tailwind升级v4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,16 +3,18 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
+        tailwindcss(),
         AutoImport({
             resolvers: [ElementPlusResolver()],
         }),
         Components({
             resolvers: [ElementPlusResolver()],
         }),
-    ]
+    ],
 })
