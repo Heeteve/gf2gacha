9f8b862a4f7da5cba926c03ea8d704e48d5a1892
Matcha <admin@mcc.wiki>
2025-03-08 17:38:45 +0800

    style:import el-message-box style

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
+import 'element-plus/theme-chalk/el-loading.css'
+import 'element-plus/theme-chalk/el-message-box.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
