db28b068f668a9a9b6a62d7dca3f4ac237a4ca70
Heeteve <30897530+Heeteve@users.noreply.github.com>
2025-08-09 13:48:35 +0800

    feat: 显示皮肤池
    1. 新版本判断修改为“最新版本大于当前版本”
    2. 显示皮肤池，只统计衣装

--- model/pool.go
+++ model/pool.go
@@ -4,10 +4,10 @@ type Pool struct {
 	PoolType        int64           `json:"poolType"`
 	GachaCount      int64           `json:"gachaCount"`
 	LoseCount       int64           `json:"loseCount"`
-	GuaranteesCount int64           `json:"guaranteesCount"`
+	GuaranteesCount int64           `json:"guaranteesCount"` // 吃保底数量
 	Rank5Count      int64           `json:"rank5Count"`
 	Rank4Count      int64           `json:"rank4Count"`
 	Rank3Count      int64           `json:"rank3Count"`
-	StoredCount     int64           `json:"storedCount"`
+	StoredCount     int64           `json:"storedCount"` // 已垫数量
 	RecordList      []DisplayRecord `json:"recordList"`
 }
